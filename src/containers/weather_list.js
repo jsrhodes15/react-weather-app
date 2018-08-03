30909daa71689903251982ec4d86a4b5d4b9e2f5
Jordan Rhodes <jsrhodes15@gmail.com>
2018-08-03 11:07:21 -0400

    Move old app to CRA setup

---
+++ src/containers/weather_list.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * (9/5) - 459.67);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    // This (below) is destructuring. Pulling these two properties off of the 'coord' property
+    // The two properties in the brackets have to be identical to the properties in
+    // cityData.city.coord
+    // Same as const lon = cityData.city.coord.lon (same for lat)
+    const { lon, lat } = cityData.city.coord;
+
+    return (
+      <tr key={name}>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="F" /></td>
+        <td><Chart data={pressure} color="green" units="hPa" /></td>
+        <td><Chart data={humidity} color="black" units="%" /></td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+// The above is a bit of ES6 syntact sugar. Same as:
+//  function mapStateToProps(state) {
+//    return {weather: state.weather}
+//  }
+
+export default connect(mapStateToProps)(WeatherList);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// Import this middleware for handling AJAX requests
+import ReduxPromise from 'redux-promise';
+// Import logger middleware
+import createLogger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/app';
+import reducers from './reducers';
+
+const loggerMiddleware = createLogger();
+// This applies middleware to the store
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise, loggerMiddleware)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
